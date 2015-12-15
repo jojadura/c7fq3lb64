e69c9ccefdfe2d55d0357fb801ca2c05cfcf1670
John Jairo <jojadura@hotmail.com>
2015-12-14 20:00:07 -0500

    References user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215004150) do
+ActiveRecord::Schema.define(version: 20151215005558) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20151215004150) do
   end
 
   add_index "expenses", ["category_id"], name: "index_expenses_on_category_id"
+  add_index "expenses", ["user_id"], name: "index_expenses_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
